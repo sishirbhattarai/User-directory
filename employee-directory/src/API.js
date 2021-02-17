e08ac474aee487cefa6236d6c3108d7ef7424967
Sishir <sishir.bhattarai@outlook.com>
2021-02-16 22:45:25 -0700

    added final code

--- employee-directory/src/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=50";
-const APIKEY = "&apikey=trilogy";
-
-export default {
-  search: function() {
-    return axios.get(BASEURL + APIKEY);
-  }
-};
